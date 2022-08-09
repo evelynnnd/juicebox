6538e236e3b997bcaa2add55891444135aada10d
Evelyn <evydelgado18@gmail.com>
2022-08-08 21:00:36 -0600

    testing

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@
 const { Client } = require('pg');
 
 // providing location of the database
-const client = new Client(process.env.DATABASE_URL || 'postgres://postgres@localhost:5432/juicebox-dev');
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
 //create the function for the users.
 const createUser = async ({ username, password, name, location }) => {
